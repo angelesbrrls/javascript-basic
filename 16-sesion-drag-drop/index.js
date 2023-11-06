6d55fbdf42458ec0aee9f4b8bd19292436b2f349
Ange <a.brrls@gmail.com>
2023-11-05 20:54:14 -0600

    sessions

---
+++ 16-sesion-drag-drop/index.js
@@ -0,0 +1,33 @@
+const parrafos = document.querySelectorAll(".parrafo")
+const secciones = document.querySelectorAll(".seccion")
+
+parrafos.forEach(parrafo => {
+    parrafo.addEventListener("dragstart", event => {
+        console.log("Dragging - párrafo: " + parrafo.innerText)
+        parrafo.classList.add("dragging")
+        event.dataTransfer.setData("id", parrafo.id)
+        const elemento_fantasma = document.querySelector(".image-fantasm")
+        event.dataTransfer.setDragImage(elemento_fantasma, 0, 0)
+    })
+
+    parrafo.addEventListener("dragend", () => {
+        // console.log("Termina arrastre")
+        parrafo.classList.remove("dragging")
+    })
+})
+
+secciones.forEach(seccion => {
+    seccion.addEventListener("dragover", event => {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = "copy" // efecto copy
+        // console.log("DragOver")
+    })
+
+    seccion.addEventListener("drop", event => {
+        console.log("Dropped")
+        const id_parrafo = event.dataTransfer.getData("id")
+        // console.log("Párrafo id: ", id_parrafo)
+        const parrafo = document.getElementById(id_parrafo)
+        seccion.appendChild(parrafo)
+    })
+})
